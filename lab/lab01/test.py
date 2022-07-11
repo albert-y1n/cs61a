964fd8170aa3ed8586d715a765497df6f80879d4
ustchunzi <yinchenlong@mail.ustc.edu.cn>
2022-07-12 02:31:20 +0800

    如此美妙的开局

---
+++ lab/lab01/test.py
@@ -0,0 +1,22 @@
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    r = 0
+    while (y//10) > 0:
+        r += y%10
+        y = y//10
+    r += y%10
+    return r
+
+sum_digits(114514)
